0a2f88a7238d9138f143d54e0081decdbc829487
DavidBai <baiminghan@gmail.com>
2021-08-21 16:18:13 +1200

    initialize framer motion.

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -5,25 +5,51 @@ import home1 from "../img/home1.png";
 
 // import styled from "styled-components";
 
+// framer motion
+
+import { motion } from "framer-motion";
+
 import { About, Description, Image, Hide } from "../style";
 
 const AboutSection = () => {
+  // // framer motion vairants
+
+  // const titleAnim = {
+  //   hidden: { opacity: 0 },
+  //   show: { opacity: 1, transition: { duration: 2 } },
+  // };
+
+  // // when use staggerChildren inside the parent, it will automatically stagger the animation in the parent and the children together.
+
+  // const container = {
+  //   hidden: { x: 100 },
+  //   show: {
+  //     x: 0,
+  //     transition: { duration: 0.75, ease: "easeOut", staggerChildren: 1 },
+  //   },
+  // };
+
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div
+        // variants={container}
+        // initial="hidden"
+        // animate="show"
+        // className="title"
+        >
           <div className="hide">
-            <h2>We work to make</h2>
+            <motion.h2>We work to make</motion.h2>
           </div>
           <Hide>
-            <h2>
+            <motion.h2>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
 
         <p>
           Contact us for any photography or videography ideas that you have. We
