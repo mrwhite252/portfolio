3a6e16130f5674e23e364d64eed20ca2e8e9f57f
DavidBai <baiminghan@gmail.com>
2021-08-21 17:14:29 +1200

    initialize the page transition

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -4,13 +4,22 @@ import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/faqSection";
 
+// import animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
+
 const AboutUs = () => {
   return (
-    <>
+    <motion.div
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      exit="exit"
+    >
       <AboutSection />
       <ServicesSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 };
 
