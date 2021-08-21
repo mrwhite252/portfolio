0b58e7cdd978b0f8bf1ad0f51c3b006c3599728b
DavidBai <baiminghan@gmail.com>
2021-08-21 19:02:11 +1200

    "about" page animation with framer motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -9,6 +9,8 @@ import home1 from "../img/home1.png";
 
 import { motion } from "framer-motion";
 
+import { titleAnim, fade, photoAnim } from "../animation";
+
 import { About, Description, Image, Hide } from "../style";
 
 const AboutSection = () => {
@@ -32,33 +34,28 @@ const AboutSection = () => {
   return (
     <About>
       <Description>
-        <motion.div
-        // variants={container}
-        // initial="hidden"
-        // animate="show"
-        // className="title"
-        >
-          <div className="hide">
-            <motion.h2>We work to make</motion.h2>
-          </div>
+        <motion.div>
           <Hide>
-            <motion.h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
+          </Hide>
+          <Hide>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
             </motion.h2>
           </Hide>
           <Hide>
-            <motion.h2>true.</motion.h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
         </motion.div>
 
-        <p>
+        <motion.p variants={fade}>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
-        </p>
-        <button>Contact Us</button>
+        </motion.p>
+        <motion.button variants={fade}>Contact Us</motion.button>
       </Description>
       <Image>
-        <img src={home1} alt="contact-us-img" />
+        <motion.img variants={photoAnim} src={home1} alt="contact-us-img" />
       </Image>
     </About>
   );
