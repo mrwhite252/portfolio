75d0c1396f6d8b1c66a6884c96619eac3609ecb0
DavidBai <baiminghan@gmail.com>
2021-08-22 19:48:50 +1200

    responsive settings.

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -78,6 +78,10 @@ const Awards = styled.div`
   margin: 5rem 10rem;
   align-items: center;
   justify-content: space-around;
+  @media (max-width: 1300px) {
+    display: block;
+    margin: 2rem 2rem;
+  }
 `;
 
 // award component
